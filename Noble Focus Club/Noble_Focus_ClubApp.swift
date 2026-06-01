17a85021635302c775cce4f141eee157a013487b
keshew <artyom.korotckov2017@yandex.ru>
2026-06-01 21:42:09 +0500

    Initial Commit

---
+++ Noble Focus Club/Noble_Focus_ClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Noble_Focus_ClubApp.swift
+//  Noble Focus Club
+//
+//  Created by Артём Коротков on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Noble_Focus_ClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
